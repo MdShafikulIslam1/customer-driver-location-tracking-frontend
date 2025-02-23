ad4b5b710f1dc884a35b8d51a0b2e83698dccd0b
shofikul <179398ssshofik@gmail.com>
2025-02-23 16:32:47 +0600

    fix: socket deploy link

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,7 +17,7 @@ import { useEffect, useRef, useState } from "react";
 
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://bkend.solar-ict.com", {
   transports: ["websocket", "polling"],
 });
 
